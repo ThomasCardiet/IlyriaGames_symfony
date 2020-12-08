70588ab46700527c6bbf1489d05201e82150aa4d
Thomas <thom_car@yahoo.fr>
2020-12-08 20:14:37 +0100

    :bug: bugfix
    - add logs colors

--- src/AppBundle/ServerInformations.php
+++ src/AppBundle/ServerInformations.php
@@ -44,12 +44,12 @@ class ServerInformations {
     }
 
     function getLogs(){
-        $log = "";
+        $logs = "";
         try {
-            $log = file_get_contents($this->path . '\\' . $this->server . '\logs\latest.log');
+            $logs = file_get_contents($this->path . '\\' . $this->server . '\logs\latest.log');
         } catch (\Exception $e) {}
 
-        return $log;
+        return $this->redefineLogColors($logs);
     }
 
     function isOnline(){
@@ -65,6 +65,37 @@ class ServerInformations {
         } catch (\Exception $e) {
             return false;
         }
+    }
+
+    function redefineLogColors($logs){
+
+        //[0;
+        $logs = preg_replace("/\\[0;30;22m/", "<span style='color: #000000;'>", $logs); //0
+        $logs = preg_replace("/\\[0;34;22m/", "<span style='color: #0000AA;'>", $logs); //1
+        $logs = preg_replace("/\\[0;32;22m/", "<span style='color: #00AA00;'>", $logs); //2
+        $logs = preg_replace("/\\[0;36;22m/", "<span style='color: #00AAAA;'>", $logs); //3
+        $logs = preg_replace("/\\[0;31;22m/", "<span style='color: #AA0000;'>", $logs); //4
+        $logs = preg_replace("/\\[0;35;22m/", "<span style='color: #AA00AA;'>", $logs); //5
+        $logs = preg_replace("/\\[0;33;22m/", "<span style='color: #FFAA00;'>", $logs); //6
+        $logs = preg_replace("/\\[0;37;22m/", "<span style='color: #AAAAAA;'>", $logs); //7
+        $logs = preg_replace("/\\[0;30;1m/", "<span style='color: #555555;'>", $logs);  //8
+        $logs = preg_replace("/\\[0;34;1m/", "<span style='color: #5555FF;'>", $logs);  //9
+
+        $logs = preg_replace("/\\[0;32;1m/", "<span style='color: #55FF55;'>", $logs);  //a
+        $logs = preg_replace("/\\[0;36;1m/", "<span style='color: #55FFFF;'>", $logs);  //b
+        $logs = preg_replace("/\\[0;31;1m/", "<span style='color: #FF5555;'>", $logs);  //c
+        $logs = preg_replace("/\\[0;35;1m/", "<span style='color: #FF55FF;'>", $logs);  //d
+        $logs = preg_replace("/\\[0;33;1m/", "<span style='color: #FFFF55;'>", $logs);  //e
+        $logs = preg_replace("/\\[0;37;1m/", "<span style='color: #FFFFFF;'>", $logs);  //f
+
+        $logs = preg_replace("/\\[5m/", "", $logs);
+        $logs = preg_replace("/\\[4m/", "", $logs);
+        $logs = preg_replace("/\\[9m/", "", $logs);
+        $logs = preg_replace("/\\[21m/", "", $logs);
+
+        $logs = preg_replace("/\\[m/", "</span>", $logs);
+
+        return $logs;
     }
 
 }
